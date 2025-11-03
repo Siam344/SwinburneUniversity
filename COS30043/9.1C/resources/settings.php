24f1a4863b0b86cdcde54101041b3574b9097706
Nur E Siam <134575818+Siam344@users.noreply.github.com>
2025-11-03 21:47:32 +1100

    Delete COS30043 directory

--- COS30043/9.1C/resources/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Config for database connection
-$host = "localhost";
-$username = "ducanh";
-$password = "ducanh2003";
-$dbname = "IDD";
--- COS30043/README.md
+++
@@ -1,2 +0,0 @@
-# COS30043
-Portfolio tasks of COS30043 - Interface Design and Development (May 2023)
